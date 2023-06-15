41e1d5981721a6c528dd23e0c88d75e8796be4e2
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2023-06-16 05:07:07 +0900

    複数のh1,buttonのrGB変更

--- colorChangePage/app.js
+++ colorChangePage/app.js
@@ -19,9 +19,10 @@
 
 
 // const container = document.querySelector(".container")
-
 const h1 = document.querySelector("h1")
+const h1s = document.querySelectorAll("h1")
 const btn = document.querySelector("#btn")
+const buttons = document.querySelectorAll("button")
 
 btn.addEventListener("click", function(){
   const r = Math.floor(Math.random() * 256);
@@ -33,3 +34,49 @@ btn.addEventListener("click", function(){
   h1.innerText = neyColor
 });
 
+
+function randomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+}
+
+for (let button of buttons){
+  button.addEventListener("click", function(){
+    this.style.backgroundColor = randomColor();
+  });
+};
+
+
+// for (let h1 of h1s){
+//   h1.addEventListener("click", function(){
+//     this.style.backgroundColor = randomColor();
+//   });
+// };
+
+
+for (let h1 of h1s){
+  h1.addEventListener("click", setColor);
+};
+
+
+// const setColor = () => {
+//     this.style.backgroundColor = randomColor();
+// }
+
+function setColor() {
+  this.style.backgroundColor = randomColor();
+};
+
+
+
+// button.addEventListener("click", function(){
+//   const r = Math.floor(Math.random() * 256);
+//   const g = Math.floor(Math.random() * 256);
+//   const b = Math.floor(Math.random() * 256);
+//   const neyColor = `rgb(${r},${g},${b})`
+
+//   document.body.style.backgroundColor = neyColor;
+//   h1.innerText = neyColor
+// });
