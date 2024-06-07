0481c0777772f11feeb1745c39e52397b78c43b6
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-06-08 06:59:38 +0900

    474. cookie-parserミドルウェア

--- Nodejs/CookiesDemo/index.js
+++ Nodejs/CookiesDemo/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.send('やっほー');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.get('/great', (req, res) => {
+    const {name = 'Anonymous', age} = req.cookies;
+    res.send(`こんにちは${name}さん。あなたは${age}歳で登録されています`);
 })
 
-app.get('/setname', (req, res) =>{
+app.get('/setname', (req, res) => {
     res.cookie('name', 'YamadaTaro');
+    res.cookie('age', 20);
     res.send('Cookie送ったよ');;
 })
 
-app.listen(3000, (req, res) =>{
+app.listen(3000, (req, res) => {
     console.log('Server起動');
 })
