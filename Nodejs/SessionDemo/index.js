bf5a361ca08043d732af20a95cdeefd0370b29b9
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-06-09 06:27:12 +0900

    480. 続・express-session

--- Nodejs/SessionDemo/index.js
+++ Nodejs/SessionDemo/index.js
@@ -2,10 +2,14 @@ const express  = require('express');
 const app = express();
 const session = require('express-session');
 
+
+const sessionOptions = {
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: false
+};
 // ミドルウェアを利用する事で、req時にconnect.sid という Keyで
-app.use(session({
-    secret: 'mysecret'
-}));
+app.use(session(sessionOptions));
 
 app.get('/viewcont', (req, res) => {
     if(req.session.count) {
@@ -16,6 +20,17 @@ app.get('/viewcont', (req, res) => {
     //ミドルウェアを利用した状態だと、cookieに｢connect.sid｣ という名前で値が登録される
     res.send(`あなたは${req.session.count}回このページを表示しました。`);
 });
+
+app.get('/register', (req, res) => {
+    const {username = 'Anonymous'} = req.query;
+    req.session.username = username;
+    res.redirect('/great');
+})
+
+app.get('/great', (req, res) => {
+    const { username } = req.session;
+    res.send(`ようこそ${username}さん`);
+})
 
 app.listen(3000, (req, res) => {
     console.log('PORT 3000 で待機中');
