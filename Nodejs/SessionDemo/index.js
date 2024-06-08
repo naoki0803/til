55b549764972bf18e0ecece686a77c0422e80e62
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-06-09 08:01:33 +0900

    482. res.localsとフラッシュ

--- Nodejs/SessionDemo/index.js
+++ Nodejs/SessionDemo/index.js
@@ -1,7 +1,14 @@
-const express  = require('express');
+const express = require('express');
 const app = express();
 const session = require('express-session');
+const flash = require('connect-flash');
 
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+app.engine("ejs", ejsMate)
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const sessionOptions = {
     secret: 'mysecret',
@@ -10,9 +17,15 @@ const sessionOptions = {
 };
 // ミドルウェアを利用する事で、req時にconnect.sid という Keyで
 app.use(session(sessionOptions));
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.messages = req.flash('success');
+    next();
+})
+
 
 app.get('/viewcont', (req, res) => {
-    if(req.session.count) {
+    if (req.session.count) {
         req.session.count += 1;
     } else {
         req.session.count = 1;
@@ -22,16 +35,21 @@ app.get('/viewcont', (req, res) => {
 });
 
 app.get('/register', (req, res) => {
-    const {username = 'Anonymous'} = req.query;
+    const { username = 'Anonymous' } = req.query;
     req.session.username = username;
+
+    //flashを利用して初回だけ表示される値を設定する
+    req.flash('success', '新規にusernameが登録されました');
     res.redirect('/great');
 })
 
 app.get('/great', (req, res) => {
     const { username } = req.session;
-    res.send(`ようこそ${username}さん`);
+    // res.send(`ようこそ${username}さん${req.flash('success')}`);
+    // res.render('./index', {username: username, messages: req.flash('success') }); //res.localsを利用しない場合の記述
+    res.render('index', {username: username});    //res.localsを利用する場合の記述
 })
 
 app.listen(3000, (req, res) => {
     console.log('PORT 3000 で待機中');
-}); {}
+});
