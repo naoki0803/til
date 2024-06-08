5b72d777cf4b2d4b37c7c1c36e057a4258f10ae9
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2024-06-09 05:32:27 +0900

    475. 署名付きクッキーに、コメントを追記

--- Nodejs/CookiesDemo/index.js
+++ Nodejs/CookiesDemo/index.js
@@ -9,22 +9,27 @@ app.get('/great', (req, res) => {
     res.send(`こんにちは${name}さん。あなたは${age}歳で登録されています`);
 })
 
+// res.cookieで、Serverからクライアントのブラウザに対してクッキーを渡す事ができる。
+//複数記述する場合は、複数行res.cookieを記載する。
 app.get('/setname', (req, res) => {
     res.cookie('name', 'YamadaTaro');
     res.cookie('age', 20);
     res.send('Cookie送ったよ');
 })
 
+// res.cookieの第二引数にオブジェクトで\{ shigned: true }を渡す事で、署名付きのCookieを渡す事ができる。
 app.get('/getsignedcookie', (req, res) => {
     res.cookie('fruit', 'grape', { signed: true });
     res.send('署名付きCookie送ったよ');
 })
 
+// 署名付きのcookieをServer側で受け取る場合、req.cookiesでは受け取れず、req.signedCookiesと記述することで受け取れる
 app.get('/veryfifruit', (req, res) => {
-    console.log(req.cookies);
-    console.log(req.signedCookies);
+    console.log("req.cookies: ", req.cookies);   //req.cookies:  {}
+    console.log("req.signedCookies: ", req.signedCookies);  //req.signedCookies:  [Object: null prototype] { fruit: 'grape' }
     res.send(req.signedCookies);
 })
+
 app.listen(3000, (req, res) => {
     console.log('Server起動');
 })
